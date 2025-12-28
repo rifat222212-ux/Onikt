b319984374102b239e0da0217811713f3ce367e1
rifat222212-ux <rifat222212@gmail.com>
2025-12-28 22:44:51 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -26,10 +26,10 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			welcomeMessage: "Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help",
+			welcomeMessage: "আমাকে আপনার গ্রুপে আনার জন্য ধন্যবাদ 🫶!\nBot prefix: %1\nTo view the list of commands, please enter: %1help",
 			multiple1: "you",
 			multiple2: "you guys",
-			defaultWelcomeMessage: `Hello {userName}.\nWelcome {multiple} to the chat group: {boxName}\nHave a nice {session} 😊`
+			defaultWelcomeMessage: `Assalamu Alaikum {userName}.\nWelcome {multiple} to the chat group: {boxName}\nenjoy the group {session} ❤️‍🩹🫵🫶`
 		}
 	},
 
