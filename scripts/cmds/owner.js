d7ce15ba8d17b38d788063efa4339ea7688498e7
rifat222212-ux <rifat222212@gmail.com>
2025-12-30 08:12:57 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -16,12 +16,12 @@ module.exports = {
 	onStart: async function ({ api, event }) {
 		try {
 			const ownerInfo = {
-				name: '𝑬𝒘𝑹 Onik ',
+				name: ' Oɴɪᴋ x Aʀɪғᴀ ',
 				gender: '𝑴𝒂𝑳𝒆',
-				Birthday: '17-10-𝟐𝟎𝟎5',
+				Birthday: '17-10-2005',
 				religion: '𝙄𝒔𝒍𝑨𝒎',
 				hobby: '𝑺𝒍𝒆𝒆𝑷𝒊𝒏𝑮',
-				Fb: 'https://www.facebook.com/onik ahmed',
+				Fb: 'https://www.facebook.com/ 유 Yᴏᴜʀ's Nᴏʙɪᴛᴀ 𖤍',
 				Relationship: '𝑺𝒊𝒏𝑮𝒆𝒍',
 				Height: '5"6'
 			};
